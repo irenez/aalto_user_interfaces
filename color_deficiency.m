e05f91550a7d6c5f89b42cef7938a5b8086bfffe
irene <paspatoufi@gmail.com>
2014-11-19 14:59:44 +0200

    final code
    Most likely this is how the code should look like. The challenge is to
    read the correct clr_map from the colormap.png image.

--- color_deficiency.m
+++ color_deficiency.m
@@ -1,6 +1,5 @@
-img = double(imread('tubemap.jpg'));
-[X, map] = imread('colorma.png', 'png');
-
-[idxImg, clrmap] = rgb2ind(img, map);
-
-image(idxImg)
+img = imread('tubemap.jpg');
+[X, clr_map] = imread('colormap.png');
+ind_img = rgb2ind(img, clr_map);
+imagesc(ind_img)
+axis off
