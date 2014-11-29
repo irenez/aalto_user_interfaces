015dafe5761ed00b32e5a53dd46d3c0613d99b74
irene <paspatoufi@gmail.com>
2014-11-29 22:16:14 +0200

    rename var

--- SDP_selectiontime.m
+++ SDP_selectiontime.m
@@ -1,6 +1,6 @@
 % Returns the prediction of the SDP model for a given item in a given menu with a given selection history
 
-function [sdp_pred] = SDP_selectiontime (menulength, itemposition, trials, p_i)
+function [st] = SDP_selectiontime (menulength, itemposition, trials, p_i)
 % 	menulength = the number of items in the menu
 % 	itemposition = position of the to-be-clicked target item in the menu (1 = first)
 % 	trials = total number of previous repetitions the user has carried out with this menu
@@ -21,4 +21,4 @@ function [sdp_pred] = SDP_selectiontime (menulength, itemposition, trials, p_i)
 	Hi = log(1/p_i,2);
 	t_hhi = b_hh * Hi + a_hh;
 	t_dsi = (1 - e_i) * t_vsi + e_i * t_hhi;
-	sdp_pred = t_dsi + t_pi
+	st = t_dsi + t_pi
