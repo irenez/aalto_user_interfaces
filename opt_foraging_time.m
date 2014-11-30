2b716e7841a1e6ef315b0734d2a4e197b62b4819
irene <paspatoufi@gmail.com>
2014-11-30 22:57:47 +0200

    final code

--- opt_foraging_time.m
+++ opt_foraging_time.m
@@ -1,5 +1,5 @@
 info = importdata('information_gain.csv')
-x = info.data(:,1);     % rate
+x = info.data(:,1);     % rank
 y = info.data(:,2);     % gain
 [m,n] = size(x);
 
@@ -15,15 +15,22 @@ theta(1,:) = zeros(1,n);
 % Logarithmic regression to find the optimal search time
 for i=2:Nit+1
 
-    g = theta(i-1)*log(x) + theta(1);
-    J_der = 1/m*(g - y)'*x;
-    theta(i,:) = theta(i-1,:) - alfa*J_der;
-    J(i-1) = -1/m*(y'*log(g) + (1-y')*log(1 - g));
+    h_theta = sigmoid(theta(i-1)*log(x));
+    J_der = 1/m*(h_theta - y)'*x;
+    theta(i) = theta(i-1) - alfa*J_der;
+%     J(i-1) = -1/m*(y'*log(h_theta) + (1-y')*log(1 - h_theta));
 
 end
 
+g = theta(Nit+1)*log(x);
 rate = lamda*g/(1+lamda*t_w);
+
+rate_opt = lamda*theta(Nit+1)/(1+lamda*t_w)
+
+figure(1)
 plot(rate)
-hold on
-plot(g, '--')
-hold off
+title('Rate of gain')
+
+figure(2)
+plot(g)
+title('Information gain')
